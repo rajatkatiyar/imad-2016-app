57747721cdb1bfa3a871a7ba66849f36115696ad
rajatkatiyar <rajjjat537@gmail.com>
2017-01-23 11:06:29 +0530

    [imad-console] Updates ui/script1.js

--- ui/script1.js
+++ ui/script1.js
@@ -1,1 +1,9 @@
 
+  
+                    var e=document.getElementById("poi");
+                   e.style.fontSize="50px";
+                   e.style.color="red";
+                   var e=document.getElementById("po");
+                  e.style.fontSize="25px";
+                 e.style.color="black";
+                 
